71cade137b05ca116f6110ed100caff9f6becba6
mikaelcodes <mikael.hussain05@gmail.com>
2025-04-12 13:36:40 -0400

    area and volumn calculator app

--- Projects/project area and volumn calculator/main.py
+++ Projects/project area and volumn calculator/main.py
@@ -43,7 +43,33 @@ label_result_area.pack(pady=5)
 
 
 
+label_volume_length = tk.Label(frame_right, text="Volume Length:")
+label_volume_length.pack(pady=5)
+ent_volume_length= tk.Entry(frame_right, width=20)
+ent_volume_length.pack(pady=5)
+lable_volume_width = tk.Label(frame_right, text="Volume Width:")
+lable_volume_width.pack(pady=5)
+ent_volume_width = tk.Entry(frame_right, width=20)
+ent_volume_width.pack(pady=5)
+lable_volume_depth = tk.Label(frame_right, text="Volume Depth:")
+lable_volume_depth.pack(pady=5)
+ent_volume_depth = tk.Entry(frame_right, width=20)
+ent_volume_depth.pack(pady=5)
 
+def calculate_volume(length_entry, width_entry):
+    try:
+        length = float(length_entry.get())
+        width = float(width_entry.get())
+        depth = float(ent_volume_depth.get())
+        volume = length * width * depth  # Assuming a fixed height of 10 for volume calculation
+        lable_result_volume.config(text=f"Volume: {volume}")
+    except ValueError:
+        lable_result_volume.config(text="Invalid input!")
+
+button_volume = tk.Button(frame_right, text="Calculate Volume", command=lambda: calculate_volume(ent_volume_length, ent_volume_width))
+button_volume.pack(pady=10)
+lable_result_volume = tk.Label(frame_right, text="Volume:")
+lable_result_volume.pack(pady=5)
 
 
 
